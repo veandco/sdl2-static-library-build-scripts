dfde58fe1a84c72114157f640c8a9bdf5ac0290c
Lilis Iskandar <lilis@veand.co>
2021-09-19 21:19:57 +0800

    Build compiler-rt in order to build for macOS platform
    Signed-off-by: Lilis Iskandar <lilis@veand.co>

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ apt install -y make cmake git gcc g++ libxml2-dev libssl-dev zlib1g-dev {gcc,g++
 # Needed for compiler-rt in osxcross (https://github.com/tpoechtrager/osxcross/issues/214#issuecomment-595279668)
 ln -sf /usr/bin/llvm-config-10 /usr/bin/llvm-config
 
+# macOS Only
 git clone https://github.com/tpoechtrager/osxcross
 cd osxcross/tarballs
 curl -O https://go-sdl2.veandco.sg-sin1.upcloudobjects.com/MacOSX11.3.sdk.tar.xz
@@ -13,4 +14,5 @@ curl -O https://go-sdl2.veandco.sg-sin1.upcloudobjects.com/MacOSX10.11.sdk.tar.x
 cd ..
 SDK_VERSION=11.3 ./build.sh
 SDK_VERSION=10.11 ./build.sh
+./build_compiler_rt.sh
 cd ..
